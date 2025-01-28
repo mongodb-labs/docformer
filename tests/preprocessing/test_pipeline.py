29a2a9d24b3813ff1c71178965885c34a07170e8
Thomas Rueckstiess <thomas@mongodb.com>
2025-01-29 10:12:35 +1100

    remove obsolete tests and fix import

--- tests/preprocessing/test_pipeline.py
+++ tests/preprocessing/test_pipeline.py
@@ -1,7 +1,7 @@
 import unittest
 
-from origami.preprocessing.pipelines import build_estimation_pipeline, build_prediction_pipelines
-from origami.utils.common import SequenceOrderMethod
+from origami.preprocessing.pipelines import build_prediction_pipelines
+from origami.utils.config import SequenceOrderMethod
 
 
 class TestBuildPipeline(unittest.TestCase):
@@ -33,28 +33,4 @@ class TestBuildPipeline(unittest.TestCase):
         self.assertEqual(
             [list(p)[0] for p in list(test_pipe.get_params()["steps"])],
             ["binning", "target", "tokenizer", "padding", "encoder"],
-        )
-
-    def test_build_estimation_pipeline_ordered(self):
-        pipeline = build_estimation_pipeline(n_bins=10, sequence_order=SequenceOrderMethod.ORDERED, keep_id=False)
-
-        self.assertEqual(
-            [list(p)[0] for p in list(pipeline.get_params()["steps"])],
-            ["binning", "schema", "exists", "tokenizer", "padding", "encoder"],
-        )
-
-    def test_build_estimation_pipeline_shuffled(self):
-        pipeline = build_estimation_pipeline(n_bins=10, sequence_order=SequenceOrderMethod.SHUFFLED, keep_id=False)
-
-        self.assertEqual(
-            [list(p)[0] for p in list(pipeline.get_params()["steps"])],
-            ["binning", "schema", "exists", "permuter", "tokenizer", "padding", "encoder"],
-        )
-
-    def test_build_estimation_pipeline_with_id(self):
-        pipeline = build_estimation_pipeline(n_bins=10, sequence_order=SequenceOrderMethod.SHUFFLED, keep_id=True)
-
-        self.assertEqual(
-            [list(p)[0] for p in list(pipeline.get_params()["steps"])],
-            ["binning", "id_setter", "schema", "exists", "permuter", "tokenizer", "padding", "encoder"],
         )
